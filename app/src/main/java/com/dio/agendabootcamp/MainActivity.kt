eb076aadc003c78b057383b1b22a4b6a879ef014
Edson Shideki Kokado <eskokado@gmail.com>
2021-03-24 10:39:24 -0300

    Preparing to insert an event in the agenda

--- app/src/main/java/com/dio/agendabootcamp/MainActivity.kt
+++ app/src/main/java/com/dio/agendabootcamp/MainActivity.kt
@@ -2,10 +2,16 @@ package com.dio.agendabootcamp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnSetEvent = findViewById(R.id.set_event) as Button
+        btnSetEvent.setOnClickListener {
+            
+        }
     }
 }
